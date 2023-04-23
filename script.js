df29193e92faf8cd89be7504f2accedca6f3ec2a
Malsim Dauhanosav <max_air@bk.ru>
2023-04-23 18:54:49 +0300

    feat: add function for CapsLock button. But it doesn't works correct

--- script.js
+++ script.js
@@ -14,30 +14,74 @@ window.addEventListener('DOMContentLoaded', () => {
   const keyboard = document.createElement('div');
   keyboard.classList.add('keyboard');
   keyboardWrpapper.append(keyboard);
+  let detectedOS;
+  if (navigator.userAgent.indexOf('Win') !== -1) {
+    detectedOS = 'Windows';
+  } else {
+    detectedOS = 'MacOS';
+  }
+  const os = document.createElement('div');
+  os.classList.add('os');
+  os.textContent = `Клавиатура создана для операционной системы ${detectedOS}`;
+  keyboardWrpapper.append(os);
 
-  const enKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
+  const language = document.createElement('div');
+  language.classList.add('language');
+  language.textContent = 'Для переключения языка нажмите левые Ctrl + Alt';
+  keyboardWrpapper.append(language);
+  let capsLock = false;
+  const enKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ArrowUp', 'Shift', 'Control', 'Meta', 'Alt', ' ', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'Control'];
+
+  function initKeyBoard(caps) {
+    document.querySelector('.keyboard').innerHTML = '';
+    enKeyboard.forEach((btn) => {
+      const button = document.createElement('button');
+      button.classList.add('btn');
+      if (btn === 'Delete') {
+        button.textContent = 'Del';
+      } else if (btn === 'Control') {
+        button.textContent = 'Ctrl';
+      } else if (btn === 'Meta') {
+        button.textContent = 'Win';
+      } else if (btn === 'ArrowLeft') {
+        button.textContent = '◄';
+      } else if (btn === 'ArrowRight') {
+        button.textContent = '►';
+      } else if (btn === 'ArrowDown') {
+        button.textContent = '▼';
+      } else if (btn === 'ArrowUp') {
+        button.textContent = '▲';
+      } else if (btn === 'CapsLock') {
+        button.classList.add('caps');
+        button.textContent = `${btn}`;
+      } else {
+        console.log(caps, btn[0].toUpperCase());
+        if (caps) {
+          button.textContent = `${btn[0].toUpperCase()}`;
+        } else {
+          button.textContent = `${btn}`;
+        }
+      }
+      keyboard.append(button);
+    });
+  }
+  initKeyBoard(capsLock);
 
   // document.onkeydown = function codesOfBtns(event) {
   //   event.preventDefault();
 
   //   console.log(event.key);
   //   // textarea.value += event.key;
-  // }
-  enKeyboard.forEach((btn) => {
-    const button = document.createElement('div');
-    button.classList.add('btn');
-    if (btn === 'Delete') {
-      button.textContent = 'Del';
-      button.classList.add('del');
-    } else {
-      button.textContent = `${btn}`;
+  // };
+  keyboard.addEventListener('click', (e) => {
+    if (e.target.textContent === 'CapsLock' && e.target.classList.contains('caps_active')) {
+      e.target.classList.remove('caps_active');
+      capsLock = false;
+      initKeyBoard(capsLock);
+    } else if (e.target.textContent === 'CapsLock' && !e.target.classList.contains('caps_active')) {
+      e.target.classList.add('caps_active');
+      capsLock = true;
+      initKeyBoard(capsLock);
     }
-    if (btn === 'Backspace' || btn === 'Shift' || btn === 'Shift') {
-      button.classList.add('longBtn');
-    }
-    if (btn === 'Tab') {
-      button.classList.add('tab');
-    }
-    keyboard.append(button);
   });
 });
